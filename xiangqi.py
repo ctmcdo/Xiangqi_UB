6fd17a3e75b659045e490355308fb936b71d1a46
ctmcdo <24464477+ctmcdo@users.noreply.github.com>
2024-01-07 08:43:24 +0000

    Update xiangqi.py

--- xiangqi.py
+++ xiangqi.py
@@ -1,65 +1,103 @@
 totalPoints = 9 * 10
-binomN = totalPoints - 2 # 2 generals
-maxHorsesChariotsAndCannonsOverBothSides = 2 * 3 * 2 # numSides * (h | ch | c) * numOfEachType
-binoms = [[0] * (maxHorsesChariotsAndCannonsOverBothSides + 1)] * (binomN + 1)
+
+binomN = totalPoints - 2  # 2 generals
+maxHorsesChariotsAndCannonsOverBothSides = (
+    2 * 3 * 2
+)  # numSides * #{h, ch, c} * numOfEachType
+binoms = [
+    [0] * (maxHorsesChariotsAndCannonsOverBothSides + 1) for i in range(binomN + 1)
+]
 for n in range(0, binomN + 1):
     binoms[n][0] = 1
 for n in range(1, binomN + 1):
     for k in range(1, maxHorsesChariotsAndCannonsOverBothSides + 1):
-        binoms[n][k] = binoms[n-1][k-1] + binoms[n-1][k]
+        binoms[n][k] = binoms[n - 1][k - 1] + binoms[n - 1][k]
 
 summaries = {}
+
+
+maxSoldiersPerSide = 5
 def sameSideSoldiers(numXiangqiMen, soldierPointsOccupied, perms):
-    for numSoldiers in range(0, 5 + 1):
+    for numSoldiers in range(0, maxSoldiersPerSide + 1):
         soldierPerms = 0
-        freeCols = 5 - soldierPointsOccupied
-        for squeezedSoldiers in range(max(numSoldiers - freeCols, 0),
-                soldierPointsOccupied + 1):
-            soldierPerms += binoms[soldierPointsOccupied][squeezedSoldiers] \
-                    + 2 * binoms[freeCols][numSoldiers - squeezedSoldiers]
-        k = (numXiangqiMen + numSoldiers, numSoldiers)
+        freeCols = maxSoldiersPerSide - soldierPointsOccupied
+        for squeezedSoldiers in range(
+            max(numSoldiers - freeCols, 0), soldierPointsOccupied + 1
+        ):
+            soldierPerms += (
+                binoms[soldierPointsOccupied][squeezedSoldiers]
+                + 2 * binoms[freeCols][numSoldiers - squeezedSoldiers]
+            )
+        k = (numXiangqiMen + 2 + numSoldiers, numSoldiers) # 2 for generals
         if k not in summaries:
             summaries[k] = soldierPerms * perms
         else:
             summaries[k] += soldierPerms * perms
 
+
 def ele(numXiangqiMen, genOn3_5, perms):
+    # 0 ele
     sameSideSoldiers(numXiangqiMen, 0, perms)
+    # 1 ele
     sameSideSoldiers(numXiangqiMen + 1, 0, (5 - genOn3_5) * perms)
     sameSideSoldiers(numXiangqiMen + 1, 1, 2 * perms)
+    # 2 ele
     sameSideSoldiers(numXiangqiMen + 2, 0, binoms[5 - genOn3_5][2] * perms)
     sameSideSoldiers(numXiangqiMen + 2, 1, 2 * (5 - genOn3_5) * perms)
     sameSideSoldiers(numXiangqiMen + 2, 2, perms)
 
-def adv(genOnCornerOrCentre, genOn3_5, perms):
-    ele(genOn3_5, 0, perms)
-    ele(genOn3_5, 1, (5 - genOnCornerOrCentre) * perms)
-    ele(genOn3_5, 2, binoms[5 - genOnCornerOrCentre][2] * perms)
 
-# Fill summaries
-adv(0, 1, 1)
-adv(1, 0, 5)
-adv(0, 0, 4)
+def adv(genOnCornerOrCentre, genOn3_5, perms):
+    ele(0, genOn3_5, perms)
+    ele(1, genOn3_5, binoms[5 - genOnCornerOrCentre][1] * perms)
+    ele(2, genOn3_5, binoms[5 - genOnCornerOrCentre][2] * perms)
+
+
+# Fill side summaries
+adv(False, False, 3) # 9 - 5 - 1 = 3
+adv(False, True, 1) # 3_5
+adv(True, False, 5) # 4 corners + 1 centre
 
 # n! Coefficient[(1 + x + x^2/2)^k, x^n] for k = 6, 0 <= n <= 12
 # k = 6 for h, ch, c, H, CH, C, or maxHorsesChariotsAndCannonsOverBothSides // 2
-exps = [1, 6, 36, 210, 1170, 6120, 29520, 128520, 491400, 1587600, 4082400, 7484400, 7484400]
+exps = [
+    1,
+    6,
+    36,
+    210,
+    1170,
+    6120,
+    29520,
+    128520,
+    491400,
+    1587600,
+    4082400,
+    7484400,
+    7484400,
+]
 
-minFreePoints = totalPoints - 2 * (1 + 2 * 2 + 5) # numSides * (gen + 2 * (adv or ele) + maxSoldiers)
+minFreePoints = totalPoints - 2 * (
+    1 + 2 * 2 + 5
+)  # numSides * (gen + 2 * (adv or ele) + maxSoldiers)
 horseChariotAndCannonPerms = {}
 for remPoints in range(minFreePoints, binomN + 1):
     horseChariotAndCannonPerms[remPoints] = 0
     for j in range(maxHorsesChariotsAndCannonsOverBothSides + 1):
         horseChariotAndCannonPerms[remPoints] += binoms[remPoints][j] * exps[j]
 
+
 def countSideCombo(k0, k1):
     halfb = totalPoints // 2
     summation = 0
-    for i in range(5 - k0[1]): # rem soldiers
-        for j in range(5 - k1[1]):
-            summation += binoms[halfb - (k1[0] + 1)][i] * binoms[halfb - (k0[0] + 1)][j] * \
-                    horseChariotAndCannonPerms[totalPoints - k0[0] - k1[0] - 2 - i - j]
-    return summation
+    for i in range(maxSoldiersPerSide - k0[1]):  # rem soldiers
+        for j in range(maxSoldiersPerSide - k1[1]):
+            summation += (
+                binoms[halfb - k1[0]][i]
+                * binoms[halfb - k0[0]][j]
+                * horseChariotAndCannonPerms[totalPoints - k0[0] - k1[0] - i - j]
+            )
+    return summation * summaries[k0] * summaries[k1]
+
 
 upperbound = 0
 summaryKeys = list(summaries.keys())
