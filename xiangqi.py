48ff05b4dfd4e3bdd1c714781bd864934a44a026
ctmcdo <24464477+ctmcdo@users.noreply.github.com>
2024-01-07 14:03:41 +0000

    Update xiangqi.py

--- xiangqi.py
+++ xiangqi.py
@@ -1,82 +1,93 @@
 pointsPerSide = 9 * 5
 numSides = 2
 totalPoints = numSides * pointsPerSide
+assert totalPoints == 90
 
-binomN = totalPoints - (numSides)  # 2 generals
-maxHorsesChariotsAndCannonsOverBothSides = (
+maxUnoccupiedPoints = totalPoints - (numSides)  # 2 generals
+maxHorsesChariotsAndCannonsBetweenThe2Sides = (
     numSides * 3 * 2
-)  # "" * #{h, ch, c} * numOfEachType
-binoms = [
-    [0] * (maxHorsesChariotsAndCannonsOverBothSides + 1) for i in range(binomN + 1)
+)  # "" * #{h, ch, c} * numOfEachTypeInBrackets
+assert maxHorsesChariotsAndCannonsBetweenThe2Sides == 12
+C = [  # binomial coefficients
+    [0] * (maxHorsesChariotsAndCannonsBetweenThe2Sides + 1)
+    for i in range(maxUnoccupiedPoints + 1)
 ]
-for n in range(0, binomN + 1):
-    binoms[n][0] = 1
-for n in range(1, binomN + 1):
-    for k in range(1, maxHorsesChariotsAndCannonsOverBothSides + 1):
-        binoms[n][k] = binoms[n - 1][k - 1] + binoms[n - 1][k]
+for n in range(0, maxUnoccupiedPoints + 1):
+    C[n][0] = 1
+for n in range(1, maxUnoccupiedPoints + 1):
+    for k in range(1, maxHorsesChariotsAndCannonsBetweenThe2Sides + 1):
+        C[n][k] = C[n - 1][k - 1] + C[n - 1][k]
+assert C[0][0] == 1
+assert C[0][1] == 0
+assert C[88][2] == 3828
+assert C[45][12] == 28760021745
 
-summaries = {}
+genAdvEleSameSideSoldierPerms = {}
 maxSoldiersPerSide = 5
 
 
-def sameSideSoldiers(numXiangqiMen, soldierPointsOccupiedByEles, perms):
+def sameSideSoldiers(numXiangqiMen, soldierPointsOccupiedByElephants, perms):
     for numSoldiers in range(0, maxSoldiersPerSide + 1):
         soldierPerms = 0
-        uninhibCols = (
-            maxSoldiersPerSide - soldierPointsOccupiedByEles
-        )  # uninhibCols \in [3, 5]
-        for squeezedSoldiers in range(
-            max(numSoldiers - uninhibCols, 0),
-            min(soldierPointsOccupiedByEles + 1, numSoldiers + 1),
+
+        uninhibitedCols = maxSoldiersPerSide - soldierPointsOccupiedByElephants
+        assert uninhibitedCols >= 3
+        assert uninhibitedCols <= 5
+
+        for inhibitedSoldiers in range(
+            max(numSoldiers - uninhibitedCols, 0),  # the min inhibited
+            min(numSoldiers + 1, soldierPointsOccupiedByElephants + 1),  # the max
         ):
-            uninhibSoldiers = numSoldiers - squeezedSoldiers
+            uninhibitedSoldiers = numSoldiers - inhibitedSoldiers
+            assert soldierPointsOccupiedByElephants >= inhibitedSoldiers
+            assert uninhibitedCols >= uninhibitedSoldiers
             soldierPerms += (
-                binoms[soldierPointsOccupiedByEles][squeezedSoldiers]
-                * binoms[uninhibCols][uninhibSoldiers]
-                * pow(2, uninhibSoldiers)
+                C[soldierPointsOccupiedByElephants][inhibitedSoldiers]
+                * C[uninhibitedCols][uninhibitedSoldiers]
+                * pow(2, uninhibitedSoldiers)
             )  # 2 for the point before crossing river and the one behind it
-        k = (numXiangqiMen + 1 + numSoldiers, numSoldiers)  # 1 for this side's gen
-        if k not in summaries:
-            summaries[k] = soldierPerms * perms
+        k = (numXiangqiMen + numSoldiers, numSoldiers)
+        if k not in genAdvEleSameSideSoldierPerms:
+            genAdvEleSameSideSoldierPerms[k] = soldierPerms * perms
         else:
-            summaries[k] += soldierPerms * perms
+            genAdvEleSameSideSoldierPerms[k] += soldierPerms * perms
 
 
-def ele(numXiangqiMen, genOn3_5, perms):
-    eleOccupiablePointsPerSide = 2 * 3 + 1  # 3 on each side of palace and 3_5
-    # 0 ele
-    sameSideSoldiers(numXiangqiMen, 0, perms)
-    # 1 ele
-    sameSideSoldiers(
-        numXiangqiMen + 1, 0, ((eleOccupiablePointsPerSide - 2) - genOn3_5) * perms
-    )  # elephants can occupy 2 soldier points
-    sameSideSoldiers(numXiangqiMen + 1, 1, 2 * perms)
-    # 2 ele
-    sameSideSoldiers(
-        numXiangqiMen + 2,
-        0,
-        binoms[(eleOccupiablePointsPerSide - 2) - genOn3_5][2] * perms,
-    )
-    sameSideSoldiers(
-        numXiangqiMen + 2,
-        1,
-        2 * binoms[(eleOccupiablePointsPerSide - 2) - genOn3_5][1] * perms,
-    )
-    sameSideSoldiers(numXiangqiMen + 2, 2, perms)
-
-
-def adv(genOnCornerOrCentre, genOn3_5, perms):
+def ele(numXiangqiMen, genOn35, perms):
+    eleOccupiablePointsPerSide = 2 * 3 + 1  # 3 on each side of palace and 35
     for i in range(2 + 1):
-        ele(i, genOn3_5, binoms[5 - genOnCornerOrCentre][i] * perms)
+        for j in range(i + 1):
+            sameSideSoldiers(
+                numXiangqiMen + i,
+                j,
+                C[2][j]  # elephants can occupy 2 soldier points
+                * C[
+                    (eleOccupiablePointsPerSide - 2) - genOn35
+                ][  #                             ^ same 2 points
+                    i - j
+                ]
+                * perms,
+            )
 
 
-# Fill side summaries
-adv(False, False, 3)  # 9 - 5 - 1 = 3
-adv(False, True, 1)  # 3_5
-adv(True, False, 5)  # 4 corners + 1 centre
+def gen(numXiangqiMen, perms):
+    ele(
+        numXiangqiMen + 1, False, (3 * 3 - numXiangqiMen - 1) * perms
+    )  #                           palace is 3 x 3 grid, in this case less #advisors and 35
+    #                                                                       (rank 3, row 5)
+    ele(numXiangqiMen + 1, True, perms)
 
-# n distinct balls (points) into k distinct bins (piece type & colour) where
-# each bin has capacity 2 (there are at most 2 horses, chariots and cannons of each colour).
+
+# Compute genAdvEleSameSideSoldierPerms
+for i in range(2 + 1):  # advisors
+    gen(i, C[5][i])  # 4 corners + 1 centre
+
+
+# We're interested in the number of ways to place n distinct balls (points / sites) into k
+# distinct bins (piece types which include the piece colour) where each bin has a capacity
+# of 2 (there are at most 2 horses, chariots and cannons of each colour).
+# This is given by an exponential generating function.
+# The following can be calculated by a computer algebra system like Wolfram:
 # n! Coefficient[(1 + x + x^2/2)^k, x^n] for k = 6, 0 <= n <= 12
 exps = [
     1,
@@ -94,39 +105,52 @@ exps = [
     7484400,
 ]
 
-minFreePoints = totalPoints - numSides * (
+minUnoccupiedPointsWithOnlyGenAdvEleAndSoldiers = totalPoints - numSides * (
     1 + 2 * 2 + maxSoldiersPerSide
 )  # "" * (gen + #{adv, ele} * numOfEachType + "")
 horseChariotAndCannonPerms = {}
-for remPoints in range(minFreePoints, binomN + 1):
-    horseChariotAndCannonPerms[remPoints] = 0
-    for j in range(maxHorsesChariotsAndCannonsOverBothSides + 1):
-        horseChariotAndCannonPerms[remPoints] += binoms[remPoints][j] * exps[j]
+for numUnoccupiedPoints in range(
+    minUnoccupiedPointsWithOnlyGenAdvEleAndSoldiers, maxUnoccupiedPoints + 1
+):
+    horseChariotAndCannonPerms[numUnoccupiedPoints] = 0
+    for balls in range(maxHorsesChariotsAndCannonsBetweenThe2Sides + 1):
+        horseChariotAndCannonPerms[numUnoccupiedPoints] += (
+            C[numUnoccupiedPoints][balls] * exps[balls]
+        )
 
 
-def countSideCombo(s0, s1):
+def sumSides(s0, s1):
+    side0_numXiangqiMen = s0[0]
+    side1_numXiangqiMen = s1[0]
+    numXiangqiMen = s0[0] + s1[0]
+    side0_numSameSideSoldiers = s0[1]
+    side1_numSameSideSoldiers = s1[1]
     summation = 0
-    for freeSoldiers0 in range(maxSoldiersPerSide - s0[1] + 1):  # rem soldiers
-        for freeSoldiers1 in range(maxSoldiersPerSide - s1[1] + 1):
+    for crossedSoldiers0 in range(maxSoldiersPerSide - side0_numSameSideSoldiers + 1):
+        for crossedSoldiers1 in range(
+            maxSoldiersPerSide - side1_numSameSideSoldiers + 1
+        ):
             summation += (
-                binoms[pointsPerSide - s1[0]][freeSoldiers0]
-                * binoms[pointsPerSide - s0[0]][freeSoldiers1]
+                C[pointsPerSide - side1_numXiangqiMen][crossedSoldiers0]
+                * C[pointsPerSide - side0_numXiangqiMen][crossedSoldiers1]
                 * horseChariotAndCannonPerms[
-                    totalPoints - s0[0] - s1[0] - freeSoldiers0 - freeSoldiers1
+                    totalPoints - numXiangqiMen - crossedSoldiers0 - crossedSoldiers1
                 ]
             )
-    return summation * summaries[s0] * summaries[s1]
+    return (
+        summation
+        * genAdvEleSameSideSoldierPerms[s0]
+        * genAdvEleSameSideSoldierPerms[s1]
+    )
 
 
-upperbound = 0
-summaryKeys = list(summaries.keys())
-for i in range(len(summaries)):
-    upperbound += countSideCombo(summaryKeys[i], summaryKeys[i])  # sideToMove
-    for j in range(i + 1, len(summaries)):
-        upperbound += numSides * countSideCombo(
-            summaryKeys[i], summaryKeys[j]
-        )  # symmetry
-print(upperbound)  # diagrams
+diagram_upperbound = 0
+sideKeys = list(genAdvEleSameSideSoldierPerms.keys())
+for i in range(len(genAdvEleSameSideSoldierPerms)):
+    diagram_upperbound += sumSides(sideKeys[i], sideKeys[i])
+    for j in range(i + 1, len(genAdvEleSameSideSoldierPerms)):
+        diagram_upperbound += numSides * sumSides(sideKeys[i], sideKeys[j])  # symmetry
+print(f"Diagrams upperbound: {diagram_upperbound}")  # diagrams
 # 7583767311308936928441671793917387439659
-print(upperbound * 2)  # sideToMove -> positions
+print(f"Positions upperbound: {2 * diagram_upperbound}")  # sideToMove -> positions
 # 15167534622617873856883343587834774879318
